a0af3bb260a608ebee97d40455083329f3e486c8
Matt Dean <matt.dean.001@gmail.com>
2017-03-16 15:52:06 +0000

    First push

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const PAGES = {
+  page1: path.join(__dirname, 'src/react-pages/page1.js'),
+};
+
+const commonConfig = {
+  entry: {
+    page1: PAGES.page1,
+  },
+  output: {
+    path: path.join(__dirname, 'build/public/js/bundles'),
+    filename: '[name].js',
+  },
+};
+
+
+module.exports = function(env) {
+  console.log('env', env);
+
+  return commonConfig;
+};
